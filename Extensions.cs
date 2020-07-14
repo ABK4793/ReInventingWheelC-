3fe301e0eb4f663fa8e156cb6c8c4541924ef3b9
ABK4793 <54903431+ABK4793@users.noreply.github.com>
2020-07-14 13:44:10 +0530

    Extensions_Deepcopy & TraceLine(Need work in Path)
    Extensions_Deepcopy & TraceLine(Need work in Path)

---
+++ Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace DesignPatterns
+{
+    /// <summary>
+    /// Binary Formatter Enforces Serilizable on it's classes,
+    /// Xml Formatter Enforces default Constructor on it's though
+    /// </summary>
+    public static partial class Extensions
+    {
+        public static T DeepCopy_BinaryFormatter<T>(T obj)
+        {
+            var stream = new MemoryStream();
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, obj);
+            stream.Seek(0, SeekOrigin.Begin);
+            object copy = formatter.Deserialize(stream);
+            stream.Close();
+            return (T) copy;
+        }
+        
+        public static T DeepCopy_XML<T>(T obj)
+        {
+            using var stream = new MemoryStream();
+            var xmlFormatter = new XmlSerializer(typeof(T));
+            xmlFormatter.Serialize(stream, obj);
+            stream.Position = 0;
+            return (T)xmlFormatter.Deserialize(stream);
+        }
+    }
+}
---
+++ TraceLine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Diagnostics;
+using System.IO;
+
+namespace DesignPatterns
+{
+    internal class TraceLine
+    {
+        private string _filePath;
+
+        [Obsolete("Old Trace Initialization",true)]
+        private void _Initialize(string filename = "Mylog.log")
+        {
+            var directoryInfo = Directory.GetParent(Environment.CurrentDirectory).Parent;
+            if (directoryInfo != null)
+                Trace.Listeners.Add(new TextWriterTraceListener(
+                    $"{Path.Combine(directoryInfo.FullName, filename)}"));
+        }
+
+        private void _InitializeToFile(string fileName = "Mylog.log")
+        {
+            var directoryInfo = Directory.GetParent(Environment.CurrentDirectory).Parent;
+            _filePath = $"{Path.Combine(directoryInfo.FullName, fileName)}";
+           
+
+        }
+        public TraceLine()
+        {
+            _InitializeToFile();
+        }
+
+        public TraceLine(string filePath= "MyLog.log")
+        {
+            _InitializeToFile(filePath);
+        }
+
+        [Obsolete("Discontinued Trace",true)]
+        public void WriteTrace(string msg, bool omitDate = false)
+        {
+
+
+            if (!omitDate)
+                msg = DateTime.Now + ":" + msg;
+            Trace.WriteLine(msg);
+        }
+
+        public void WriteLine(string msg, bool omitDate = false)
+        {
+            if (!omitDate)
+                msg = DateTime.Now + ":" + msg;
+            using (FileStream fs = new FileStream(_filePath, FileMode.Append, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                sw.WriteLine(msg);
+            }
+        }
+    }
+}
